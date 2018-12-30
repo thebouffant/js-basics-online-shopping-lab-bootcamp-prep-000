a43dc63b8b0fd3e2c67f50d993181f4e93c666a1
Marc <marc.troughton@gmail.com>
2018-12-30 21:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 
 function addToCart(item) {
 cart.push({itemName: item, itemPrice: getRandomInt()})
-return `${cart.itemName} has been added to your cart.`
+return `${item} has been added to your cart.`
 }
 
 function getRandomInt() {
